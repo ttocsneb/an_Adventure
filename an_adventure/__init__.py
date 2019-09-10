d55140ae2e69ca0976c1834818a993b868554dca
Benjamin Jacobs <benjammin1100@gmail.com>
2019-09-10 17:42:46 -0600

    test curses

--- an_adventure/__init__.py
+++ an_adventure/__init__.py
@@ -1,4 +1,17 @@
+import curses
+# https://docs.python.org/3/howto/curses.html
+
+import time
+
+def main(stdscr):
+    curses.start_color()
+    stdscr.clear()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE)
+    for i in reversed(range(1, 11)):
+        stdscr.addstr(10-i, 0, '10 divided by {} is {}'.format(i, 10/i), curses.color_pair(1))
+        stdscr.refresh()
+        time.sleep(1)
 
 
 def start():
-    print("hello World!")
+    curses.wrapper(main)
