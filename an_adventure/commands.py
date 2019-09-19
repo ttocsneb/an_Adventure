2a47cdde779a52dc33fe22d928ddb159e310f15f
Kyle Baron <kyle.b.baron@gmail.com>
2019-09-19 11:41:14 -0600

    Respond function added

--- an_adventure/commands.py
+++ an_adventure/commands.py
@@ -4,7 +4,63 @@ from colorama import Fore
 from adventurelib import say, when, Bag, Item
 import time
 import random
+from pymaybe import maybe
 from . import globalvars
+
+
+def respond(obj, attr, default):
+    if hasattr(obj, attr):
+        print(getattr(obj, attr))
+    else:
+        print(default)
+
+
+@when ('eat ITEM')
+def eat(item):
+    obj = globalvars.save_data.player.find(item)
+    if not obj:
+        print(f'You do not have a {item}.')
+    elif maybe(obj).edible == True:
+        globalvars.save_data.player.take(item)
+        respond(obj, 'eat_succ', f'You eat the {obj}.')
+    else:
+        respond(obj, 'eat_fail', f'You can\'t eat the {obj}.')
+        
+
+
+@when('inventory')
+def inventory():
+    print('You have:')
+    if not globalvars.save_data.player:
+        print('nothing')
+        return
+    for item in globalvars.save_data.player:
+        print(f'* {item}')
+
+
+@when("take ITEM")
+def take(item):
+    obj = globalvars.save_data.current_room.items.take(item)
+    if not obj:
+        print(f'there is no {item} here')
+    elif maybe(obj).immovable == True:
+        globalvars.save_data.current_room.items.add(obj)
+        respond(obj, 'take_fail', f'You cannot take the {obj}.')
+    else:    
+        globalvars.save_data.player.add(obj)
+        respond(obj, 'take_succ', f'You have taken a {obj}.')
+
+
+@when('look at ITEM')
+def lookat(item):
+    obj = maybe(globalvars.save_data.player.find(item))
+    if obj.is_none():
+        print(f"You do not have a {item}.")
+    else:
+        if obj.color.is_some():
+            print(f"It's a sort of {obj.color}-ish colour")
+        if obj.size.is_some():
+            print(f"it's about {obj.size}")
 
 
 @when("brush teeth")
